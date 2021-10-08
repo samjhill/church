766f7f4c3374c2af93182b75fb8381810a2a1fd2
Sam Hill <samuhill@gmail.com>
2021-10-08 11:08:41 -0400

    rebuild

--- src/components/flamingo-god.jsx
+++ src/components/flamingo-god.jsx
@@ -4,8 +4,8 @@ function SvgComponent(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-    //   width={1253.333}
-    //   height={1858.667}
+      width={1253.333}
+      height={1858.667}
       viewBox="0 0 940 1394"
       {...props}
     >
