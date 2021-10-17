b779b5d364a9a37ffb6169dcfd24bf67f1946936
Sam Hill <samuhill@gmail.com>
2021-10-17 09:35:31 -0400

    attempt to fix manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,10 @@ module.exports = {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png",
+        name: "Flamingo Church",
+        short_name: `Church`,
+        start_url: `/`,
+        display: `standalone`,
       },
     },
     "gatsby-plugin-mdx",
@@ -24,11 +28,12 @@ module.exports = {
       __key: "pages",
     },
     {
-        resolve: 'gatsby-plugin-mailchimp',
-        options: {
-            endpoint: 'https://flamingochurch.us5.list-manage.com/subscribe/post?u=6ebe0b4af973c20b095e1eb03&amp;id=f896297f9e', 
-            timeout: 3500,
-        },
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint:
+          "https://flamingochurch.us5.list-manage.com/subscribe/post?u=6ebe0b4af973c20b095e1eb03&amp;id=f896297f9e",
+        timeout: 3500,
+      },
     },
   ],
 };
